7375522dce9b37f539ec2da94a9c11c6ae57cd6f
imsanur <imsa>
2017-05-20 21:05:47 +0700

    coding data ktp

---
+++ pbojava_e-ktp/src/pbojava_e/ktp/DataKTP.java
@@ -0,0 +1,112 @@
+package pbojava_e.ktp;
+
+public class DataKTP {
+    private String NIK;
+    private String nama;
+    private String TTL;
+    private String Jkl;
+    private String alamat;
+    private String rt;
+    private String desa;
+    private String kecamatan;
+    private String agama;
+    private String status;
+    private String pekerjaan;
+    private String kewarganegaraan;
+
+    public String getNIK() {
+        return NIK;
+    }
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTTL() {
+        return TTL;
+    }
+
+    public void setTTL(String TTL) {
+        this.TTL = TTL;
+    }
+
+    public String getJkl() {
+        return Jkl;
+    }
+
+    public void setJkl(String Jkl) {
+        this.Jkl = Jkl;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getRt() {
+        return rt;
+    }
+
+    public void setRt(String rt) {
+        this.rt = rt;
+    }
+
+    public String getDesa() {
+        return desa;
+    }
+
+    public void setDesa(String desa) {
+        this.desa = desa;
+    }
+
+    public String getKecamatan() {
+        return kecamatan;
+    }
+
+    public void setKecamatan(String kecamatan) {
+        this.kecamatan = kecamatan;
+    }
+
+    public String getAgama() {
+        return agama;
+    }
+
+    public void setAgama(String agama) {
+        this.agama = agama;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPekerjaan() {
+        return pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.pekerjaan = pekerjaan;
+    }
+
+    public String getKewarganegaraan() {
+        return kewarganegaraan;
+    }
+
+    public void setKewarganegaraan(String kewarganegaraan) {
+        this.kewarganegaraan = kewarganegaraan;
+    }
+}
